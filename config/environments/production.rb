f3868bb8910e0d17d7f0ab668f7e3348ab82c597
giorgio-paoloni <gio.paoloni@gmail.com>
2022-11-05 16:39:53 +0100

    sistemato bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   #idem developemtn, ma solo google
-  config.active_storage.service = :google
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
