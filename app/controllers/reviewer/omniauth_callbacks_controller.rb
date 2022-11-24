6b72bfb372f6449716ff50fdd24c9777baf2fe4b
LeonardoTTI <leonardo.mugnetti.00@gmail.com>
2022-11-24 11:02:00 +0100

    Google side oauth ok

--- app/controllers/reviewer/omniauth_callbacks_controller.rb
+++ app/controllers/reviewer/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Reviewer::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
       if reviewer.present?
         sign_out_all_scopes
         flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect reviewer, event: :authentication
+        sign_in_and_redirect @reviewer, event: :authentication
       else
         flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
         redirect_to new_reviewer_session_path
--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -13,5 +13,23 @@ class Reviewer < ApplicationRecord
     has_one_attached :profile_picture, service: :google
     validates :profile_picture,  blob: { content_type: :image }
     
+
+    def self.create_from_provider_data(provider_data)
+        where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
+          user.email = provider_data.info.email
+          user.password = Devise.friendly_token[0, 20]
+        end
+    end
+
+    def self.from_google_params
+        @from_google_params ||= {
+          uid: auth.uid,
+          email: auth.info.email
+        }
+     end
+  
+     def auth
+        @auth ||= request.env['omniauth.auth']
+     end
     #https://github.com/aki77/activestorage-validator
 end
