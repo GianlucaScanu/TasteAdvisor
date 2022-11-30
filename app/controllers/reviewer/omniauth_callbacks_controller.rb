ff99a1237ae905b2f39063645c4d1deb5adac93c
giorgio-paoloni <gio.paoloni@gmail.com>
2022-11-30 23:44:55 +0100

    google oauth funzionante, da sistemare

--- app/controllers/reviewer/omniauth_callbacks_controller.rb
+++ app/controllers/reviewer/omniauth_callbacks_controller.rb
@@ -1,27 +1,16 @@
 class Reviewer::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def google_oauth2
-      #https://stackoverflow.com/questions/66960490/argumenterror-wrong-number-of-arguments-in-google-oauth-callback
-      #reviewer = Reviewer.from_google(**from_google_params)
-      reviewer = Reviewer.from_google2(auth)
 
-      if reviewer.present?
-        sign_out_all_scopes
-        flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect reviewer, event: :authentication
-      else
-        flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
-        redirect_to new_reviewer_session_path
-      end
+  def google_oauth2
+    reviewer = Reviewer.from_google(auth)
+
+    if reviewer.present?
+      sign_out_all_scopes
+      flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect reviewer, event: :authentication
+    else
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+      redirect_to new_reviewer_session_path
     end
-
-  def from_google_params
-    #prende i campi utili da auth e fa il parsing 
-    @from_google_params ||= {
-      uid: auth.uid,
-      email: auth.info.email,
-      #full_name: auth.info.name,
-      #avatar_url: auth.info.image
-    }
   end
 
   def auth
--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -13,36 +13,28 @@ class Reviewer < ApplicationRecord
     has_one_attached :profile_picture, service: :google
     validates :profile_picture,  blob: { content_type: :image }
     
-
-    def self.create_from_provider_data(provider_data)
-        where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
-          user.email = provider_data.info.email
-          user.password = Devise.friendly_token[0, 20]
-        end
-    end
-
-    def self.from_google(email:, uid:)
-      #return nil unless email =~ /@mybusiness.com\z/
-      #create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = email
-        user.password = Devise.friendly_token[0, 20]
-        user.full_name = "lassi_test"
-      end
-    end
-
-    def self.from_google2(auth)
+    def self.from_google(auth)
       #https://www.youtube.com/watch?v=CnZnwV38cjo
       #https://github.com/Deanout/omniauth_google/blob/main/app/models/user.rb
-      #return nil unless email =~ /@mybusiness.com\z/
-      #create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+      #CAMPI auth https://github.com/zquestz/omniauth-google-oauth2
       where(provider: auth.provider, uid: auth.uid).first_or_create do |reviewer|
-      reviewer.email = auth.info.email
-      #username = email
-      reviewer.username = auth.info.email
-      reviewer.password = Devise.friendly_token[0, 20]
-      #reviewer.full_name = auth.info.name # assuming the user model has a name
-      #user.avatar_url = auth.info.image # assuming the user model has an image
+        #campi da assegnare al modello reviewer, tramite oauth
+        #testati
+        reviewer.email = auth.info.email
+        reviewer.password = Devise.friendly_token[0, 20]
+        reviewer.username = auth.info.first_name.to_s
+        reviewer.username +="-"+ auth.info.last_name.to_s
+        reviewer.username +="-"
+        reviewer.username += auth.info.email.to_s
+        reviewer.uid = auth.uid
+        reviewer.provider = auth.provider
+
+        #non funziona
+        #va caricato su GCS, secondo me non rileva il link corretto e da errore
+        #reviewer.profile_picture = auth.info.image
+
+        #reviewer.full_name = auth.info.name # assuming the user model has a name
+        #user.avatar_url = auth.info.image # assuming the user model has an image
       end
     end
   
