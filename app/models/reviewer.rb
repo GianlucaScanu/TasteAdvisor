a07545d020d660fff7835737dc3dbaee41cff29e
giorgio-paoloni <gio.paoloni@gmail.com>
2022-12-01 17:28:20 +0100

    google auth funzionante

--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -14,30 +14,34 @@ class Reviewer < ApplicationRecord
     validates :profile_picture,  blob: { content_type: :image }
     
     def self.from_google(auth)
-      #https://www.youtube.com/watch?v=CnZnwV38cjo
-      #https://github.com/Deanout/omniauth_google/blob/main/app/models/user.rb
-      #CAMPI auth https://github.com/zquestz/omniauth-google-oauth2
       where(provider: auth.provider, uid: auth.uid).first_or_create do |reviewer|
         #campi da assegnare al modello reviewer, tramite oauth
-        #testati
-        reviewer.email = auth.info.email
-        reviewer.password = Devise.friendly_token[0, 20]
-        reviewer.username = auth.info.first_name.to_s
-        reviewer.username +="-"+ auth.info.last_name.to_s
-        reviewer.username +="-"
-        reviewer.username += auth.info.email.to_s
         reviewer.uid = auth.uid
         reviewer.provider = auth.provider
-
-        #non funziona
-        #va caricato su GCS, secondo me non rileva il link corretto e da errore
-        #reviewer.profile_picture = auth.info.image
-
-        #reviewer.full_name = auth.info.name # assuming the user model has a name
-        #user.avatar_url = auth.info.image # assuming the user model has an image
+        reviewer.email = auth.info.email
+        reviewer.password = Devise.friendly_token[0, 20]
+        #creo un username, tecnicamente univoco, in maniera incrementale per disponibilità
+        i = -1
+        s_username = auth.info.first_name.to_s << "-" << auth.info.last_name.to_s
+        t_username = s_username
+        while (Reviewer.exists?(username: t_username))
+          i += 1
+          t_username = s_username + i.to_s
+        end
+        reviewer.username = t_username
+        reviewer.profile_picture.attach(io: URI.parse(auth.info.image).open, filename: "profile_picture.jpg")
+        reviewer.description = "Ciao mi chiamo " << auth.info.name << " e sono registrato tramite account Google! "
       end
     end
-  
-
-    #https://github.com/aki77/activestorage-validator
 end
+
+#per eliminare l'utente e ritestarlo (ID = numero dell'id utente, es.8) scriverlo su qualsiasi pagina (es.index.html.erb)
+#<%#=Reviewer.find(ID).destroy%> 
+#l'id si può vedere tramite /reviewers/
+
+#FONTI E GUIDE SEGUITE:
+#https://github.com/aki77/activestorage-validator
+#https://www.youtube.com/watch?v=CnZnwV38cjo
+#https://github.com/Deanout/omniauth_google/blob/main/app/models/user.rb
+#CAMPI auth https://github.com/zquestz/omniauth-google-oauth2
+#https://stackoverflow.com/questions/51027995/how-to-save-an-image-from-a-url-with-rails-active-storage
