a4d41d34ac63c0b9fe01f029fe71248bede21970
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2022-12-09 23:39:20 +0100

    aggiunte per us

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TasteAdvisor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.action_view.preload_links_header = false
 
     # Configuration for the application, engines, and railties goes here.
     #
